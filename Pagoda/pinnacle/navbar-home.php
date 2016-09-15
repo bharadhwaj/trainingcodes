a00e75f2bda9d7f905c09036fbf315b671126e9b
bharadhwaj <bharadhwaj10@gmail.com>
2016-09-15 12:18:27 +0530

    Responsive header image

--- Pagoda/pinnacle/navbar-home.php
+++ Pagoda/pinnacle/navbar-home.php
@@ -1,15 +1,17 @@
+<?php include_once "controller/setup/dbconfig.php" ?>
+
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <a class="navbar-brand navbar-alignit" href="/">
+        <img alt="Brand" src="assets/images/pinnacle_lgo_white.png">
+      </a>
+      <button align="center" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">
-        <img alt="Brand" src="assets/images/pinnacle_lgo_white.png">
-      </a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
--- Pagoda/pinnacle/navbar.php
+++ Pagoda/pinnacle/navbar.php
@@ -1,18 +1,17 @@
-
-<nav class="navbar navbar-default">
 <?php include_once "controller/setup/dbconfig.php" ?>
 
+<nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
+      <a class="navbar-brand" href="/">
+        <img alt="Brand" src="assets/images/pinnacle_lgo_black.png">
+      </a>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">
-        <img alt="Brand" src="assets/images/pinnacle_lgo_black.png">
-      </a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
