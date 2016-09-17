d4582fdbf2e83c328f08b653817bf3799b72fc36
bharadhwaj <bharadhwaj10@gmail.com>
2016-09-18 00:24:56 +0530

    Added 403 page

---
+++ Pagoda/trains/error403.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+
+    <title>Pinnacle | Error 403</title>
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/textstyles.css" rel="stylesheet">
+    <link href="assets/css/navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="errorpage-text">
+      <strong>403 Error!</strong>
+    </div>
+    <div class="subheading-text">
+    <strong>You don't have permission to access this folder!</strong> <br> 
+    <strong> <a href="/">Click here</a> to go back to home!</strong>
+    </div>
+    <div align="center">
+    <img src="assets/images/403.png">
+    </div>
+
+   
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <script type="text/javascript" src="assets/js/jquery.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
